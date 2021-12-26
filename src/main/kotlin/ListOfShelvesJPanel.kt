19248f4fba00dacb4dd0cede75161f4b783d03aa
shmueldabomb441 <60489781+shmueldabomb441@users.noreply.github.com>
2021-12-26 09:12:01 -0500

    Create ListOfShelvesJPanel.kt

---
+++ src/main/kotlin/ListOfShelvesJPanel.kt
@@ -0,0 +1,9 @@
+class ListOfCategoriesJPanel : SearchableTableJPanel("Enter name of category:") {
+    override val originalCollection: Collection<Any>
+        get() = Catalog
+            .entries
+            .mapTo(mutableSetOf()) { it.shelfNum }
+    override val listBeingDisplayed: MutableList<Any> = originalCollection.toMutableList()
+    override val displayingCatalogEntry: Boolean = false
+    override val columns: List<String> = listOf("Shelf")
+}
