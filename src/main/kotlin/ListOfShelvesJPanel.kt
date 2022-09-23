5dcdf7781657f6df255f245047f0f7eaf5186686
shmueldabomb441 <60489781+shmueldabomb441@users.noreply.github.com>
2022-09-23 17:20:30 -0400

    Add column for categories stored on shelf

--- src/main/kotlin/ListOfShelvesJPanel.kt
+++ src/main/kotlin/ListOfShelvesJPanel.kt
@@ -1,8 +1,12 @@
 class ListOfShelvesJPanel : SearchableTableJPanel("Enter number of shelf$alternatePhrases:") {
     override val originalCollection: Collection<Any>
-        get() = Catalog.entries
-            .mapTo(mutableSetOf()) { it.shelfNum }
+        get() =
+            Catalog
+                .entries
+                .mapValueToKey({ it.shelfNum }, { it.category }) { categories -> categories.joinToString() }
+                .map { it.value to it.key }
     override val listBeingDisplayed: MutableList<Any> = originalCollection.toMutableList().toSynchronizedList()
     override val displayingCatalogEntry: Boolean = false
-    override val columns: List<String> = listOf("Shelf")
+    override val columns: List<String> = listOf("Categories on shelf", "Shelf")
+    override val getElementToSearchBy: (Pair<*, *>) -> String = { it.second as String }
 }
