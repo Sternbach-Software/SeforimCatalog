41a2457e60a5c91e79acd461d6089f8cd3aab474
shmueldabomb441 <60489781+shmueldabomb441@users.noreply.github.com>
2022-09-23 17:21:52 -0400

    Add column for shelves that contain that category

--- src/main/kotlin/ListOfCategoriesJPanel.kt
+++ src/main/kotlin/ListOfCategoriesJPanel.kt
@@ -1,8 +1,12 @@
 class ListOfCategoriesJPanel : SearchableTableJPanel("Enter name of category$alternatePhrases:") {
     override val originalCollection: Collection<Any>
-        get() = Catalog.entries
-            .mapTo(mutableSetOf()) { it.category }
+        get() =
+            Catalog
+                .entries
+                .mapValueToKey({ it.category }, { it.shelfNum }) { shelves -> shelves.sortedWith(shelfNumComparator).joinToString() }
+                .map { it.value to it.key }
     override val listBeingDisplayed: MutableList<Any> = originalCollection.toMutableList().toSynchronizedList()
     override val displayingCatalogEntry: Boolean = false
-    override val columns: List<String> = listOf("Category")
+    override val columns: List<String> = listOf("Shelves that contain category", "Category")
+    override val getElementToSearchBy: (Pair<*, *>) -> String = { it.second as String }
 }
