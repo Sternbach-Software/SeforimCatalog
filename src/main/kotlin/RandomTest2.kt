8e3b45ea7242cb34a8965f791089ada8961ba774
shmueldabomb441 <shmuelsternbach613@gmail.com>
2022-10-21 17:35:29 -0400

    Random test

--- src/main/kotlin/RandomTest2.kt
+++ src/main/kotlin/RandomTest2.kt
@@ -1,14 +1,39 @@
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import java.io.File
 import java.nio.file.Files
+import java.util.concurrent.atomic.AtomicInteger
 
-fun main() {
-    val list = mutableListOf<String>()
-    val regex = "(?<=(Nanoseconds taken to get shiurID: ))\\d+".toRegex()
-    Files.readAllLines(File("/Users/shmuel/AndroidStudioProjects/public-android/raw files/random.txt").toPath())
-        .forEach {
-//            val find = it.find(regex)
-//            if (find.value != null) list.add(find.value)
+fun main() = runBlocking {
+    class Foo(var a: Int, var b: Int)
+    val foo1 = Foo(1,2)
+    val foo2 = Foo(3,4)
+    val list1 = mutableListOf<Foo>()
+    val list2 = mutableListOf<Foo>()
+    list1.add(foo1)
+    list1.add(foo2)
+    for(item in list1) list2.add(item)
+    println(list2[0].a)
+    list2[0].a = 0
+    println(list1[0].a == 0)
+    println(list2[0] === foo1)
+    println(list2[1] === foo2)
+    println(list2[0] === foo2)
+    println(list2[1] === foo1)
+    val flow = flow {
+        val int = AtomicInteger()
+//        while(true) {
+            delay(500)
+            emit(int.incrementAndGet())
+//        }
+    }
+    launch {
+        flow.collect {
+            println(it)
         }
-    File("/Users/shmuel/AndroidStudioProjects/public-android/raw files/random_output.txt")
-        .writeText(list.joinToString("\n"))
+    }
+    println("Got here")
 }
